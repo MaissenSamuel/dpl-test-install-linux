467f00e57d1c36fe392bd22e62ce172f07774707
MaissenSamuel <146180190+MaissenSamuel@users.noreply.github.com>
2024-06-01 13:37:55 +0200

    Update install-script-post-snapshot.sh

--- install-script-post-snapshot.sh
+++ install-script-post-snapshot.sh
@@ -1,11 +1,27 @@
 #! /bin/bash
 
-sleep 30
-echo "Hello World" > /dpl_tmp/post_snapshot.txt
+echo "Starting with post installation script"
 
-multiflags=`cat /dpl_tmp/multiflags.txt`  
+### Retrieval of multiflags and credentials
+multiflags=`cat /dpl_tmp/multiflags.txt`
+credentials=`cat /dpl_tmp/credentials.txt`
+
+
+
+### EXAMPLE: Flag handling 
+##Flag by index
+# write-host $($multiflags[0].flag)
+##Flag by name
+# write-host $multiflags.Where({$_.Name -like "flag name 1"}).flag
 echo "$multiflags" 
 
 
-#CLEANUP
+### CLEANUP Files
+rm /dpl_tmp/multiflags.txt
+rm /dpl_tmp/credentials.txt
+## Optional: Remove installation source
+# Remove-Item -Path C:\Windows\Temp\REPOSITORY_NAME.txt
 
+### Cleanup Artifacts
+## Clear history
+history -c
